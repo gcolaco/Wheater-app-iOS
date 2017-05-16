b184e141138acbf9292c4ea68c4bfdba5ccf5c63
gcolaco <gustavo.colaco@hotmail.com>
2017-05-16 13:22:05 -0300

    Class & Constants for some dates

---
+++ WeatherApp/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  WeatherApp
+//
+//  Created by Gustavo Colaco on 16/05/17.
+//  Copyright © 2017 Gustavo Colaco. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+let LATIDUDE = "lat="
+let LONGITUDE = "&lon="
+let APP_ID = "&appid="
+let API_KEY = "e114ef7182c350110ebe3735dd2f072f"
+
+let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATIDUDE)35\(LONGITUDE)139\(APP_ID)\(API_KEY)"
+
+
