680fe7ec8186e0aeba304579fc198f5deddd4401
gcolaco <gustavo.colaco@hotmail.com>
2017-05-15 21:46:59 -0300

    pods added & IBOutlet added

---
+++ WeatherApp/WeatherVC.swift
@@ -0,0 +1,28 @@
+//
+//  WeatherVC.swift
+//  WeatherApp
+//
+//  Created by Gustavo Colaco on 15/05/17.
+//  Copyright © 2017 Gustavo Colaco. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
